6ec7708a9d0bc9b66c249985a77d9b5aa3a5ade1
Vivien L'Helguen <95030000+OrmeSile@users.noreply.github.com>
2024-02-18 12:46:29 +0100

    cleanup

--- keycloak-old/src/main/java/dev/orme/oauth2/keycloak/config/RegularJsonConfigProviderFactory.java
+++
@@ -1,7 +0,0 @@
-package dev.orme.oauth2.keycloak.config;
-
-import org.keycloak.services.util.JsonConfigProviderFactory;
-
-public class RegularJsonConfigProviderFactory extends JsonConfigProviderFactory {
-
-}
