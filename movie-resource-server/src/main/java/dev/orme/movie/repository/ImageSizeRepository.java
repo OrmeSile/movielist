4e76bff923c974bd9bef488584a8cc4f7c3ed652
Vivien L'Helguen <95030000+OrmeSile@users.noreply.github.com>
2024-03-04 18:18:05 +0100

    added cache

--- movie-resource-server/src/main/java/dev/orme/movie/repository/ImageSizeRepository.java
+++ movie-resource-server/src/main/java/dev/orme/movie/repository/ImageSizeRepository.java
@@ -2,11 +2,15 @@ package dev.orme.movie.repository;
 
 import dev.orme.movie.entity.ImageSize;
 import dev.orme.movie.entity.keys.ImageSizeCompositeKey;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.Optional;
 import java.util.Set;
 
 public interface ImageSizeRepository extends CrudRepository<ImageSize, ImageSizeCompositeKey> {
+    @Cacheable(value = "imageSize")
     Optional<Set<ImageSize>> findAllByKey(String key);
+
 }
