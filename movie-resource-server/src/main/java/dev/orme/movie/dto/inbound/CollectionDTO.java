9fe24077f7459c9c137efe13dca77e69954761ba
Vivien L'Helguen <95030000+OrmeSile@users.noreply.github.com>
2024-03-04 16:36:57 +0100

    error fixes

--- movie-resource-server/src/main/java/dev/orme/movie/dto/inbound/CollectionDTO.java
+++ movie-resource-server/src/main/java/dev/orme/movie/dto/inbound/CollectionDTO.java
@@ -1,8 +1,8 @@
-package dev.orme.movie.dto;
+package dev.orme.movie.dto.inbound;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 @JsonDeserialize
-public record GenreDTO(int id, String name) {}
+public record CollectionDTO(int id, String name, String poster_path, String backdrop_path) {}
--- movie-resource-server/src/main/java/dev/orme/movie/dto/inbound/ConfigurationDTO.java
+++ movie-resource-server/src/main/java/dev/orme/movie/dto/inbound/ConfigurationDTO.java
@@ -1,4 +1,4 @@
-package dev.orme.movie.dto;
+package dev.orme.movie.dto.inbound;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
@@ -6,5 +6,6 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import java.util.Set;
 @JsonAutoDetect(fieldVisibility = JsonAutoDetect.Visibility.ANY)
 @JsonDeserialize
-public record GenreListRequestDTO(Set<GenreDTO> genres) {
+public record ConfigurationDTO(ImagesConfigurationDTO images, Set<String> change_keys) {
 }
+
